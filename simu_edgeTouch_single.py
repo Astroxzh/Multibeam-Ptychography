db82cddb17f8a6ac562d19390f487a4edefca051
Daniel S. Penagos Molina <santiago.penagos@uni-jena.de>
2025-05-06 11:11:22 +0200

    simplifies logic

--- simu_edgeTouch_single.py
+++ simu_edgeTouch_single.py
@@ -12,70 +12,9 @@ wavelength = 584e-9 #627e-9
 k = 2 * np.pi / wavelength
 N = 8000
 
-#%%
-#square slits
-# localSuqare = np.zeros([N, N // 4], dtype=complex)
-# localSuqare[100 : -100, N // 4 // 4 : N // 4 - N // 4 // 4] = 1
-# lightsource = np.tile(localSuqare, [1,4])
-# lightsource = convolve2d(lightsource, utils.gaussian2D(5, 1).astype(np.float32), mode="same")
-
-#light source
-lightsourceSize = 10e-3
-lightsourcedx = lightsourceSize / N
-lightsourcex = np.arange(- N / 2 , N / 2) * lightsourcedx
-[lightsourceX, lightsourceY] = np.meshgrid(lightsourcex, lightsourcex)
-translationCoor = [lightsourceX ]
-coorTran = [lightsourcex[0]*1000, lightsourcex[-1]*1000, lightsourcex[0]*1000, lightsourcex[-1]*1000]
-# propagator = utils.angularPropagator(30,wavelength=wavelength, dx=lightsourcedx, N=N)
-# initialField = utils.angularPro(lightsource, propagator)
-# initialField = lightsource
-initialField = np.ones([N, N])
-
-#%% 
-#illuminate lens
-
-dz = 10e-3  # distance to mask
-
-# lensdx = wavelength * dz / 4
-# lensSize = lensdx * N
-# lensx = np.arange(- N / 2, N / 2) * lensdx
-# [lensX, lensY] = np.meshgrid(lensx, lensx)
-
-# lens and aperture
-lensSize = 9e-3
-lensFocuLength = 20e-3
-apertureLens = utils.circ(lightsourceX, lightsourceY, lensSize)
-initialWave = initialField
-
-# lensTF = np.exp(-1.0j * k * (lensX**2 + lensY**2) / (2 * lensFocuLength))
-lensTF = np.exp(-1.0j * k * (lightsourceX**2 + lightsourceY**2) / (2 * lensFocuLength))
-# plt.figure()
-# plt.imshow(np.angle(lensTF))
-exitWaveWrap = initialWave * lensTF #unwrap is not helping
-exitWavePhase = np.unwrap(np.unwrap(np.angle(exitWaveWrap), axis=0), axis=1)
-# plt.figure()
-# plt.imshow(exitWavePhase, extent=coorTran)
-exitWave = np.abs(exitWaveWrap) * np.exp(1j * exitWavePhase)
-exitWave *= apertureLens
-# plt.figure()
-# plt.imshow(np.angle(exitWave), extent=coorTran)
-
-#%%
-
-#propagate to mask
-propagatorMask = utils.angularPropagator(dz=dz, wavelength=wavelength, N=N, dx=lightsourcedx)
-illuMask = np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(exitWave))*propagatorMask))
-# illuMask = utils.fresnelPro(exitWave, wavelength=wavelength, dz=dz, dx=lightsourcedx)
-# illuMask = exitWave 
-
-coorTran = [lightsourcex[0]*1000, lightsourcex[-1]*1000, lightsourcex[0]*1000, lightsourcex[-1]*1000]
-# plt.figure()
-# plt.imshow(np.abs(illuMask)**2, extent=coorTran)
-
-#%%
 #create mask (spiral aperture)
 maskSize = 4e-3
-maskN = int(N / (lightsourceSize / maskSize))
+maskN = N
 maskdx = maskSize / maskN
 
 maskx = np.arange(-maskN / 2, maskN / 2) * maskdx
@@ -86,7 +25,10 @@ localMaskN = maskN / maskNum
 localMaskSize = maskSize / maskNum
 localMaskdx = localMaskSize / localMaskN
 localMaskx = np.arange(-localMaskN / 2, localMaskN / 2) * localMaskdx
+totalMaskx = np.arange(-N/2, N/2)*localMaskdx
 [localMaskX, localMaskY] = np.meshgrid(localMaskx, localMaskx)
+[totalMaskX, totalMaskY] = np.meshgrid(totalMaskx, totalMaskx)
+
 apertureSize = 200e-6
 numOfMask = 8
 # aperture = utils.circ(localMaskX, localMaskY, apertureSize)
@@ -95,58 +37,150 @@ numOfMask = 8
 mask = np.fliplr(utils.maskGeneration(numOfMask=numOfMask, wavelength=wavelength, f=7.5e-3, N=localMaskN, dx=localMaskdx, blades_diameter=apertureSize, angle=180))
 mask = np.pad(mask, (N-maskN)//2)
 # mask = np.tile(aperture, [4, 4])
-[maskX, maskY] = np.meshgrid(lightsourcex, lightsourcex)
-# plt.figure()
-# plt.imshow(np.abs(mask), extent=coorTran)
+[maskX, maskY] = np.meshgrid(localMaskx, localMaskx)
+coorTran = [totalMaskx[0]*1000, totalMaskx[-1]*1000, totalMaskx[0]*1000, totalMaskx[-1]*1000]
+plt.figure(figsize=(4,4), dpi=100)
+plt.imshow(np.abs(mask), extent=coorTran, cmap='gray')
+plt.xlabel('mm')
+plt.ylabel('mm')
+plt.title('mask')
+plt.tight_layout()
+plt.show()
+
+#
+#%%
+#Illumination params
+f=20e-3
+dm=10e-3
+illu_wavefront = np.exp(-1.0j * k * (totalMaskX**2 + totalMaskY**2) / (2 * (f-dm)))
+# plt.figure(figsize=(4,4), dpi=100)
+# plt.imshow(utils.complex2rgb(illu_wavefront), extent=coorTran)
 # plt.xlabel('mm')
 # plt.ylabel('mm')
-# plt.title('mask')
+# plt.title('illu_wavefront')
+# plt.tight_layout()
+# plt.show()
 
 
-exitWaveMask = illuMask * mask
-plt.figure()
-plt.imshow(np.abs(exitWaveMask)**2, extent=coorTran)
-#%%
-
-#4f system mag effect
-# demagWave = utils.demagFourier(exitWaveMask, 4)
-demagWave = exitWaveMask
-# plt.figure()
-# plt.imshow(np.abs(demagWave)**2, extent=coorTran)
-
-#%%
-#propagate to sample plane
+# #propagate to sample plane
 ds = 6e-3  # distance to sample after mask
-propagatorSample = utils.angularPropagator(dz=ds, wavelength=wavelength, N=N, dx=lightsourcedx)
-illuSample = np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(demagWave))*propagatorSample))
-# plt.figure()
-# plt.imshow(np.log(np.abs(illuSample)**2+1), extent=coorTran)
-
-# profile = (np.abs(illuSample)**2)[1570:2100, 2475]
-# plt.figure()
-# plt.plot(profile)
-# %%
-
-cmap = utils.setCustomColorMap()
-
-fig, ax = plt.subplots(2,2, figsize=(10,10))
-ax = ax.ravel()
-fig.suptitle(f'probe simu with {lensFocuLength*1000}mm foculength lens')
-ax[0].imshow(np.abs(mask), extent=coorTran)
-ax[0].set_title('mask', fontsize=8)
-ax[1].imshow(np.abs(illuMask)**2, extent=coorTran)
-ax[1].set_title(f'illumination field on mask \n {dz*1000}mm after source', fontsize=8)
-ax[2].imshow(np.abs(exitWaveMask)**2, extent=coorTran)
-ax[2].set_title('exit field after mask', fontsize=8)
-ax[3].imshow(np.log(np.abs(illuSample)**2+1), extent=coorTran)
-# ax[3].imshow(utils.complex2rgb(illuSample), extent=coorTran, cmap = cmap)
-ax[3].set_title(f'probe on sample plane \n {ds*1000}mm propagation', fontsize=8)
-# ax[3].plot([0.7, 0.7], [0.0, 1.0], color='red', linewidth=0.5)
-fig.text(0.5, 0.04, 'coordinate/mm', ha='center', fontsize=12)
-fig.text(0.1, 0.5, 'coordinate/mm', va='center', rotation='vertical', fontsize=12)
-
-
-
-
+propagated_field = utils.aspw(mask*illu_wavefront, wavelength, dx=maskdx, dz=ds)
+plt.figure(figsize=(4,4), dpi=100)
+plt.imshow(np.log10(abs(propagated_field)+1), extent=coorTran, cmap=utils.setCustomColorMap())
+plt.xlabel('mm')
+plt.ylabel('mm')
+plt.title('propagated field')
+plt.tight_layout()
 plt.show()
+
+# propagatorSample = utils.angularPropagator(dz=ds, wavelength=wavelength, N=N, dx=lightsourcedx)
+
+#square slits
+# localSuqare = np.zeros([N, N // 4], dtype=complex)
+# localSuqare[100 : -100, N // 4 // 4 : N // 4 - N // 4 // 4] = 1
+# lightsource = np.tile(localSuqare, [1,4])
+# lightsource = convolve2d(lightsource, utils.gaussian2D(5, 1).astype(np.float32), mode="same")
+
+#light source
+# lightsourceSize = 10e-3
+# lightsourcedx = lightsourceSize / N
+# lightsourcex = np.arange(- N / 2 , N / 2) * lightsourcedx
+# [lightsourceX, lightsourceY] = np.meshgrid(lightsourcex, lightsourcex)
+# translationCoor = [lightsourceX ]
+# coorTran = [lightsourcex[0]*1000, lightsourcex[-1]*1000, lightsourcex[0]*1000, lightsourcex[-1]*1000]
+# # propagator = utils.angularPropagator(30,wavelength=wavelength, dx=lightsourcedx, N=N)
+# # initialField = utils.angularPro(lightsource, propagator)
+# # initialField = lightsource
+# initialField = np.ones([N, N])
+#
+# #%%
+# #illuminate lens
+#
+# dz = 10e-3  # distance to mask
+#
+# # lensdx = wavelength * dz / 4
+# # lensSize = lensdx * N
+# # lensx = np.arange(- N / 2, N / 2) * lensdx
+# # [lensX, lensY] = np.meshgrid(lensx, lensx)
+#
+# # lens and aperture
+# lensSize = 9e-3
+# lensFocuLength = 20e-3
+# apertureLens = utils.circ(lightsourceX, lightsourceY, lensSize)
+# initialWave = initialField
+#
+# # lensTF = np.exp(-1.0j * k * (lensX**2 + lensY**2) / (2 * lensFocuLength))
+# lensTF = np.exp(-1.0j * k * (lightsourceX**2 + lightsourceY**2) / (2 * lensFocuLength))
+# # plt.figure()
+# # plt.imshow(np.angle(lensTF))
+# exitWaveWrap = initialWave * lensTF #unwrap is not helping
+# exitWavePhase = np.unwrap(np.unwrap(np.angle(exitWaveWrap), axis=0), axis=1)
+# # plt.figure()
+# # plt.imshow(exitWavePhase, extent=coorTran)
+# exitWave = np.abs(exitWaveWrap) * np.exp(1j * exitWavePhase)
+# exitWave *= apertureLens
+# # plt.figure()
+# # plt.imshow(np.angle(exitWave), extent=coorTran)
+#
+# #%%
+#
+# #propagate to mask
+# propagatorMask = utils.angularPropagator(dz=dz, wavelength=wavelength, N=N, dx=lightsourcedx)
+# illuMask = np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(exitWave))*propagatorMask))
+# # illuMask = utils.fresnelPro(exitWave, wavelength=wavelength, dz=dz, dx=lightsourcedx)
+# # illuMask = exitWave
+#
+# coorTran = [lightsourcex[0]*1000, lightsourcex[-1]*1000, lightsourcex[0]*1000, lightsourcex[-1]*1000]
+# # plt.figure()
+# # plt.imshow(np.abs(illuMask)**2, extent=coorTran)
+#
+# #%%
+#
+#
+# exitWaveMask = illuMask * mask
+# plt.figure()
+# plt.imshow(np.abs(exitWaveMask)**2, extent=coorTran)
+# #%%
+#
+# #4f system mag effect
+# # demagWave = utils.demagFourier(exitWaveMask, 4)
+# demagWave = exitWaveMask
+# # plt.figure()
+# # plt.imshow(np.abs(demagWave)**2, extent=coorTran)
+#
+# #%%
+# #propagate to sample plane
+# ds = 6e-3  # distance to sample after mask
+# propagatorSample = utils.angularPropagator(dz=ds, wavelength=wavelength, N=N, dx=lightsourcedx)
+# illuSample = np.fft.ifft2(np.fft.ifftshift(np.fft.fftshift(np.fft.fft2(demagWave))*propagatorSample))
+# # plt.figure()
+# # plt.imshow(np.log(np.abs(illuSample)**2+1), extent=coorTran)
+#
+# # profile = (np.abs(illuSample)**2)[1570:2100, 2475]
+# # plt.figure()
+# # plt.plot(profile)
+# # %%
+#
+# cmap = utils.setCustomColorMap()
+#
+# fig, ax = plt.subplots(2,2, figsize=(10,10))
+# ax = ax.ravel()
+# fig.suptitle(f'probe simu with {lensFocuLength*1000}mm foculength lens')
+# ax[0].imshow(np.abs(mask), extent=coorTran)
+# ax[0].set_title('mask', fontsize=8)
+# ax[1].imshow(np.abs(illuMask)**2, extent=coorTran)
+# ax[1].set_title(f'illumination field on mask \n {dz*1000}mm after source', fontsize=8)
+# ax[2].imshow(np.abs(exitWaveMask)**2, extent=coorTran)
+# ax[2].set_title('exit field after mask', fontsize=8)
+# ax[3].imshow(np.log(np.abs(illuSample)**2+1), extent=coorTran)
+# # ax[3].imshow(utils.complex2rgb(illuSample), extent=coorTran, cmap = cmap)
+# ax[3].set_title(f'probe on sample plane \n {ds*1000}mm propagation', fontsize=8)
+# # ax[3].plot([0.7, 0.7], [0.0, 1.0], color='red', linewidth=0.5)
+# fig.text(0.5, 0.04, 'coordinate/mm', ha='center', fontsize=12)
+# fig.text(0.1, 0.5, 'coordinate/mm', va='center', rotation='vertical', fontsize=12)
+#
+#
+#
+#
+# plt.show()
 # %%
